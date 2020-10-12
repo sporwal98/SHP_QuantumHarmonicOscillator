529b2e95220954215b4d17bd911215e96a9f333a
sporwal98 <sahaj.porwal@outlook.com>
2020-10-12 09:37:50 +0100

    Add files via upload

--- program.py
+++ program.py
@@ -1,121 +1,107 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ntau = 40 #elements in time lattice
-idt = 0 #euclidean time i in {1,...,ntau}
-tmc = 0 #Monte Carlo time
-n_paths= 0 #Number of paths in ensemble
-nbins = 0 # Number of bins in Jackknife
+class Observe:
+    def __init__(self):
+        return
 
-niter = 10000#number of iterations
-dtau = 1e0#lattice spacing
+class Action:
+    '''
+    Class for action
+    '''
+    def __init__(self, m = 1, om = 1):
+        self.mass = m
+        self.omega = om
+        return
 
-xs = [] #list of x positions
-
-
-def main():
-#PLACEHOLDER
-#Main simulation code
-    x = 0 #effective(x/dtau)
-    backup_x = 0 #temporary variable for cases where change is rejected
-    ham_in = 0 #initial hamiltonian(just declaring)
-    ham_fin = 0 #final hamiltonian(just declaring)
-    metropolis = 0 #metropolis factor(just declaring
-
-    #parameters needed
-    m = 1 #mass(effective, m*dtau)
-    omega = 1 # = sqrt(k/m) where k is force constant(effective, omega*dtau
-    
-    
+    def harmonic_act(self,x):
+        #Calculation of Harmonic Oscillator Action
+        return 0.5*self.mass*self.omega* x**2
     
 
-    #initial config
-    x = 0.0
-    xs.append(x)
-    
+class Metropolis:
+    '''
+    Class for Metropolis time-step
+    '''
+    def __init__(self, ls = 1e0, act = Action()):
+        self.latticespace = ls
+        self.action = act
+        return
+
+    def step(self,x):
+        #One Metropolis step
+        acc = False
+        tmpx = x
+
+        #Equally likely to move in positive and negative direction
+        sign = np.random.random()
+        if(sign>=0.5):
+            sign = +1
+        else:
+            sign = -1
+        
+        
+        x = x + sign*self.latticespace
+        delta_act = -(self.action.harmonic_act(x)-self.action.harmonic_act(tmpx))
+        metro_factor = np.random.random()
+        
+        
+        if(np.exp(delta_act)>metro_factor):
+            acc = True
+        else:
+            x = tmpx
+        return x, acc
+
+def main(output = 'output.txt', ls = 1e0, niterations = 1000, skip = 0):
+    '''
+    Main simulation code
+    '''
+    np.random.seed(42) #random seed for consistent results
+    idt = 0 #euclidean time i in {1,...,ntau}
+    tmc = 0 #Monte Carlo timesteps
+
+    niter = niterations #number of iterations
+    latticespace = ls #increments in position(permitted positions)
+    x = 0.0 #initial position
+    tmc = 0 #monte carlo time-step
+        
+    xs = [x] #list of x positions
+    ts = [tmc] #list of time-steps
+    '''
     #open output file
     outfile = open('output.txt','w')
     outfile.write('HEADERS')
-
+    '''
+    
     naccept = 0 #counter for acceptances
-    sum_xx = 0.0 #sum of x^2, used for <x^2>
+    #sum_x = 0.0 #sum of x, used for <x>
+    #sum_xx = 0.0 #sum of x^2, used for <x^2>
 
-    for iter in range(0, niter):
-    #for each iteration
-        backup_x = x
-        x, ham_in, ham_fin = harmonic_osc(x, ham_in,ham_fin, m, omega)
-        metropolis = np.random.random()
+    act = Action(m = 1, om = 1)
+    met = Metropolis(ls = latticespace, act = act)
 
-        if(np.exp(ham_in - ham_fin) > metropolis):
-        #accept
-            naccept = naccept + 1
-        else:
-        #reject
-            x = backup_x
+    for iter in range(niter):
+    #For each monte carlo timestep
+        x, acc = met.step(x)
+        tmc += 1
 
-        sum_xx = sum_xx + x*x
+        if(acc):
+            naccept += 1
 
         xs.append(x)
-        
-        outfile.write('DATA')
+        ts.append(tmc)
 
-    outfile.close()
-    
-            
-        
+        #sum_x += x
+        #sum_xx += x^2
+        #outfile.write('DATA')
 
+    #outfile.close()
     
+    plt.clf()
+    plt.plot(xs[skip:],ts[skip:])
+    plt.xlabel('Position x')
+    plt.ylabel('Monte Carlo Timestep')
+    plt.title('Acceptance = '+str(naccept/niter))
+    plt.show()
     
     return 0
-
-def harmonic_osc(x, ham_in, ham_fin, m,omega):
-#PLACEHOLDER
-#CHANGE IN SYSTEM
-    para = 'PLACEHOLDER'
-
-    rand1 = np.random.random()
-    rand2 = np.random.random()
-
-    #p value not finalised
-    p = rand1
-    
-    ham_in = calc_ham(m, omega, x, p)
-
-    #ITERATION OF X NEEDS TO CHANGE
-    x = x + p*0.5*dtau
-    
-    for step in range(1, ntau):
-    #ITERATIONS NEED CITATION
-        delh = calc_delh(m, omega, x)
-        p = p - delh*dtau
-        x = x + p*0.5*dtau
-
-    delh = calc_delh(para)
-    p = p - delh 
-    x = x + p*0.5*dtau
-
-    ham_fin = calc_ham(m, omega, x, p)
-
-    return x, ham_in, ham_fin    
-
-    
-
-def calc_action(m, omega, x):
-#PLACEHOLDER
-#Calculation of action. Change must be reflected in calc_delh
-    act = m*omega*omega*x*x/2
-    return act
-
-def calc_ham(m, omega, x, p):
-#PLACEHOLDER
-#Calculation of action
-    ham = calc_action(m,omega,x)
-    ham = ham + p*p/(2*m)
-
-    return ham
-
-def calc_delh(m,omega,x):
-#PLACEHOLDER
-#Calculation of derivative of Hamiltonian wrt x
-    delh = m*omega*omega*x
-    return delh
