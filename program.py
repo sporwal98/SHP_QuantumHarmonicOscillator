8ca33539d24bb8950e7ca10c130cb9ff69d43718
sporwal98 <sahaj.porwal@outlook.com>
2020-10-27 13:14:22 +0000

    Add files via upload

--- program.py
+++ program.py
@@ -11,7 +11,7 @@ class Observe:
         return
 
     def selfac(self, sitepositions,timeshift, niter):
-        result = np.multiply(sitepositions,np.roll(sitepositions,timeshift*niter))        
+        result = np.multiply(sitepositions,np.roll(sitepositions,timeshift, axis = 0))        
         result = np.sum(result,axis = 0)/niter
         return result
 
@@ -19,23 +19,30 @@ class Action:
     '''
     Class for action
     '''
-    def __init__(self, m = 1, om = 1):
-        self.mass = m
+    def __init__(self, om = 1):
+        self.mass = 1
         self.omega = om
         return
 
-    def harmonic_act(self,x):
+    def harmonic_act_old(self,x):
         #Calculation of Harmonic Oscillator Action
         
-        return 0.5*self.mass*self.omega**2*x**2 #+ self.diff_actionwrtx(x-np.roll(x,1)) 
-        
+        return 0.5*self.mass*self.omega**2*x**2
 
-    def diff_actionwrtx(self,x):
-        #Differential of action wrt x
-        return self.mass*self.omega*x
-    
-    
-        
+    def harmonic_act(self, x):
+        action = 0.0
+        for i in range(len(x)):
+            action += (0.5*(x[(i+1)%len(x)]-x[i])**2)+(0.5*self.omega**2*x[i]**2)
+        return action
+
+    def del_act(self,xold,xnew,idx):
+        del_act = 0.0
+        for i in range(idx-1,idx+1):
+            oldpart = 0.5*(xold[(i+1)%len(xold)]-xold[i])**2 + 0.5*(self.omega**2)*(xold[i]**2)
+            newpart = 0.5*(xnew[(i+1)%len(xnew)]-xnew[i])**2 + 0.5*(self.omega**2)*(xnew[i]**2)
+            del_act += newpart-oldpart
+
+        return del_act
 
 class Metropolis:
     '''
@@ -46,21 +53,24 @@ class Metropolis:
         self.action = act
         return
 
-    def step(self,x):
+    def step(self,x, idx):
         #One Metropolis step
         acc = False
-        tmpx = x
+        tmpx = x.copy()
+        #print(x)
+        
 
         #Equally likely to move in positive and negative direction
         step_scale = 2*(np.random.random() - 0.5)
         
         
-        x = x + step_scale*self.stepsize
-        delta_act = -(self.action.harmonic_act(x)-self.action.harmonic_act(tmpx))
+        x[idx] = x[idx] + step_scale*self.stepsize
+        delta_act = self.action.del_act(tmpx,x,idx)
         metro_factor = np.random.random()
+        #print('dact = '+str(np.exp(delta_act))+'metro = '+str(metro_factor))
         
         
-        if(np.exp(delta_act)>metro_factor):
+        if(np.exp(-delta_act)>metro_factor):
             acc = True
         else:
             x = tmpx
@@ -76,98 +86,8 @@ class State:
         self.mass = mass
     
 
-def singlesite(output = 'output.txt', ss = 1e0, niterations = 1000, skip = 0):
-    '''
-    Main simulation code
-    '''
-    np.random.seed(42) #random seed for consistent results
-    idt = 0 #euclidean time i in {1,...,ntau}
-    tmc = 0 #Monte Carlo timesteps
 
-    niter = niterations #number of iterations
-    stepsize = ss #increments in position(permitted positions)
-    x = 0.0 #initial position
-    tmc = 0 #monte carlo time-step
-        
-    xs = [x] #list of x positions
-    ts = [tmc] #list of time-steps
-    
-    '''
-    #open output file
-    outfile = open('output.txt','w')
-    outfile.write('HEADERS')
-    '''
-    
-    naccept = 0 #counter for acceptances
-    sum_x = 0.0 #sum of x, used for <x>
-    sum_xx = 0.0 #sum of x^2, used for <x^2>
-
-    act = Action(m = 1, om = 1) #mass and omega just for generalisation
-    met = Metropolis(ss = stepsize, act = act) 
-
-    energies = [act.harmonic_act(x)]
-    
-    for iter in range(niter):
-    #For each monte carlo timestep
-        x, acc = met.step(x)
-        tmc += 1
-        
-
-        if(acc):
-            naccept += 1
-
-        xs.append(x)
-        ts.append(tmc)
-        energies.append(act.harmonic_act(x))
-        
-        sum_x += x
-        sum_xx += x**2
-        #outfile.write('DATA')
-
-    #outfile.close()
-
-    '''   
-    plt.clf()
-    plt.plot(xs[skip:],ts[skip:])
-    plt.xlabel('Position x')
-    plt.ylabel('Monte Carlo Timestep')
-    plt.title('Acceptance = '+str(naccept/niter))
-    plt.show()
-
-    plt.clf()
-    plt.plot(ts[skip:], energies[skip:])
-    plt.xlabel('Monte Carlo Timestep')
-    plt.ylabel('Action')
-    plt.title('Energies')
-    plt.show()
-    '''
-
-    mu = sum_x/niter
-    rms = sum_xx/niter
-    var = rms - (mu**2)
-
-    sigma = np.sqrt(var)
-    gaussx = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
-    normal = np.exp(-gaussx**2/2*var)/(sigma*np.sqrt(2*np.pi))
-    
-    
-    plt.clf()
-    plt.plot(gaussx, normal,color = 'r',label = 'gaussian')
-    plt.hist(xs, bins = int(np.sqrt(niter)),label = 'histogram', density = True)
-    plt.title('Position Histogram and Gaussian '+ str(niter))
-    plt.legend()
-    plt.show()
-    
-    print('<x> = ' + str(mu))
-    print('<x^2> = ' + str(rms))
-    
-    
-
-
-    
-    return 0
-
-def multisite(output = 'output.txt', ss = 1e0, niterations = 1000, ns = 10, skip = 0):
+def multisite(output = 'output.txt', ss = 1e0, niterations = 1000, ns = 10, omega = 1,skip = 0):
     #multisite monte carlo
     np.random.seed(42) #random seed for consistent results
     idt = 0 #euclidean time i in {1,...,ntau}
@@ -179,7 +99,7 @@ def multisite(output = 'output.txt', ss = 1e0, niterations = 1000, ns = 10, skip
     nsites = ns #number of sites(ntau)
     lattice = State(nsites = ns)
     
-    act = Action(m = 1, om = 1) #mass and omega just for generalisation
+    act = Action(om = omega) #mass and omega just for generalisation
     met = Metropolis(ss = stepsize, act = act) #Metropolis object
     obs = Observe() # Object for observables.
     
@@ -197,11 +117,11 @@ def multisite(output = 'output.txt', ss = 1e0, niterations = 1000, ns = 10, skip
         obs.xpos2 = np.append(obs.xpos2, lattice.positions**2)
         ts.append(tmc)
         for j in range(nsites):#for each sweep
-            x = lattice.positions[order[j]]
-            xnew, acc = met.step(x)
+            x = lattice.positions.copy()
+            xnew, acc = met.step(x,j)
             if(acc):
                 naccept[order[j]]+=1
-                lattice.positions[order[j]] = xnew
+                lattice.positions = xnew
         
         obs.sum_x +=lattice.positions
         obs.sum_xx +=lattice.positions**2
@@ -211,10 +131,11 @@ def multisite(output = 'output.txt', ss = 1e0, niterations = 1000, ns = 10, skip
     print('avg acceptance')
     print(acceptance)
     #print(obs.xpos.shape)
-    
+
+    #Setting up the gaussian plot
     mu = obs.sum_x/niter
-    rms = obs.sum_xx/niter
-    var = rms - (mu**2)
+    ms = obs.sum_xx/niter
+    var = ms - (mu**2)
 
     sigma = np.sqrt(var)
     gaussx = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
@@ -241,19 +162,22 @@ def multisite(output = 'output.txt', ss = 1e0, niterations = 1000, ns = 10, skip
         '''
         autocorrs.append(np.mean(obs.selfac(obs.xpos,i,niter)))
     autocorrs = np.array(autocorrs)
-    print(autocorrs.shape)
+    #print(autocorrs.shape)
     plt.clf()
-    plt.plot(ts[skip:],autocorrs[skip:])
+    plt.plot(ts[:50],autocorrs[:50])
+    plt.plot(ts[:50],np.zeros(50), c ='r')
     plt.xlabel('Time')
     plt.ylabel('Autocorrelation(for Time difference)')
     plt.show()
 
     plt.clf()
 
+    '''
     linefit = np.polyfit(np.mean(obs.xpos,axis = 1),autocorrs, deg = 1)
     print(linefit)
-
-    return 0
+    '''
+    
+    return
 
 
     
